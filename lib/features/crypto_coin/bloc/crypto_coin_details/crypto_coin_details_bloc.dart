ba6fde8388ec50068d1bbd82b05a99e8a8bfd18c
Temofka <basin1998@gmail.com>
2024-12-04 13:34:25 +0300

    28.11

---
+++ lib/features/crypto_coin/bloc/crypto_coin_details/crypto_coin_details_bloc.dart
@@ -0,0 +1,46 @@
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:get_it/get_it.dart';
+import 'package:talker_flutter/talker_flutter.dart';
+
+import '../../../../repositories/crypto_coins/abstarct_coins_repository.dart';
+import '../../../../repositories/crypto_coins/models/crypto_coin.dart';
+
+part 'crypto_coin_details_event.dart';
+part 'crypto_coin_details_state.dart';
+
+class CryptoCoinDetailsBloc
+    extends Bloc<CryptoCoinDetailsEvent, CryptoCoinDetailsState> {
+  CryptoCoinDetailsBloc(this.coinsRepository)
+      : super(const CryptoCoinDetailsState()) {
+    on<LoadCryptoCoinDetails>(_load);
+  }
+
+  final AbstractCoinsRepository coinsRepository;
+
+  Future<void> _load(
+    LoadCryptoCoinDetails event,
+    Emitter<CryptoCoinDetailsState> emit,
+  ) async {
+    try {
+      if (state is! CryptoCoinDetailsLoaded) {
+        emit(const CryptoCoinDetailsLoading());
+      }
+
+      final coinDetails =
+          await coinsRepository.getCoinDetails(event.currencyCode);
+
+      emit(CryptoCoinDetailsLoaded(coinDetails));
+    } catch (e, st) {
+      emit(CryptoCoinDetailsLoadingFailure(e));
+      GetIt.I<Talker>().handle(e, st);
+    }
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    super.onError(error, stackTrace);
+    GetIt.I<Talker>().handle(error, stackTrace);
+  }
+}
---
+++ lib/repositories/crypto_coins/abstarct_coins_repository.dart
@@ -0,0 +1,8 @@
+
+
+import 'models/crypto_coin.dart';
+
+abstract class AbstractCoinsRepository {
+  Future<List<CryptoCoin>> getCoinsList();
+  Future<CryptoCoin> getCoinDetails(String currencyCode);
+}
